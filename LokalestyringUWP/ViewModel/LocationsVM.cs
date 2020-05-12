83c4f5cd811be39d976943e706e21e4977b3886b
Magnus Hansen <magnus131hansen@gmail.com>
2020-05-12 12:55:51 +0200

    location

--- LokalestyringUWP/ViewModel/LocationsVM.cs
+++ LokalestyringUWP/ViewModel/LocationsVM.cs
@@ -1,12 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
+using System.Reflection.Metadata.Ecma335;
 using System.Text;
 using System.Threading.Tasks;
+using LokalestyringUWP.Models;
+using LokalestyringUWP.Service;
 
 namespace LokalestyringUWP.ViewModel
 {
-    class LocationsVM
+    public class LocationsVM
     {
+        ObservableCollection<Location> Locations { get; set; }
+        static Location selectedLocation { get; set; }
+
+        public LocationsVM()
+        {
+            Locations = new ObservableCollection<Location>();
+            LoadLocations();
+        }
+
+        public async void LoadLocations()
+        {
+            Locations = await PersistancyService.LoadLocationsFromJsonAsync();
+        }
     }
 }
