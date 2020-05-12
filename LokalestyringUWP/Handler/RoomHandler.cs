38beb98db326e8f12c69689930b4a23957a2d8ef
Anders Qvistgaard Højer <ande711e@edu.easj.dk>
2020-05-12 15:20:48 +0200

    filter

--- LokalestyringUWP/Handler/RoomHandler.cs
+++ LokalestyringUWP/Handler/RoomHandler.cs
@@ -2,6 +2,7 @@
 using LokalestyringUWP.Service;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,9 +11,27 @@ namespace LokalestyringUWP.Handler
 {
     class RoomHandler
     {
+        public ViewModel.ViewModel Reference { get; set; }
+        public RoomHandler(ViewModel.ViewModel p)
+        {
+            Reference = p;
+        }
         public static void SaveRoomsAsync(Room obj)
         {
             PersistancyService.SaveRoomAsJsonAsync(obj);
+        }
+
+        public void FilterSearchMethod()
+        {
+            var filteredList = from fl in Reference.RoomList
+                               where fl.RoomType.Contains("Lille") && fl.Building1.Contains("B")
+                               select fl;
+
+            Reference.RoomList.Clear();
+            foreach (var item in filteredList)
+            {
+                Reference.RoomList.Add(item);
+            }
         }
     }
 }
