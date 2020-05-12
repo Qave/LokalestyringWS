d7e466f2795bc2d1336cd9a8e3aa32fd5bfdc8e6
Jakob-Droid <58071936+Jakob-Droid@users.noreply.github.com>
2020-05-12 16:44:21 +0200

    Login Done?

---
+++ LokalestyringUWP/ViewModel/LoginVM.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using LokalestyringUWP.Common;
+using LokalestyringUWP.Handler;
+using LokalestyringUWP.Models;
+
+namespace LokalestyringUWP.ViewModel
+{
+    public class LoginVM
+    {
+        public string PasswordVM
+        {
+            get { return LoginHandler.Password;}
+            set { LoginHandler.Password = value; }
+        }
+
+        public string UserNameVM
+        {
+            get { return LoginHandler.UserName;}
+            set { LoginHandler.UserName = value; }
+        } 
+        
+
+       public LoginVM()
+        {
+            CommandHandler.LoginCommand = new RelayCommand(LoginHandler.OnLogin, null);
+            UserCatalogSingleton abc = UserCatalogSingleton.Instance;
+        }
+
+        public void Login()
+        {
+
+        }
+
+        public ICommand LoginCommand => CommandHandler.LoginCommand;
+    }
+}
