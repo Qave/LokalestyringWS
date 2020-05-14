0f1b09047e83abbdd2809e99e98e46521337f7e8
Kasper Stilling Andersen <kasp762s@edu.easj.dk>
2020-05-14 14:08:21 +0200

    fewfewfewr

---
+++ LokalestyringUWP/Models/Singletons/AllBookingsViewCatalogSingleton.cs
@@ -0,0 +1,32 @@
+﻿using LokalestyringUWP.Service;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LokalestyringUWP.Models.Singletons
+{
+    public class AllBookingsViewCatalogSingleton
+    {
+        private static AllBookingsViewCatalogSingleton _instance = null;
+        public ObservableCollection<AllBookingsView> AllBookings { get; }
+        public static AllBookingsViewCatalogSingleton Instance { get { return _instance ?? (_instance = new AllBookingsViewCatalogSingleton()); } }
+        public AllBookingsViewCatalogSingleton()
+        {
+            AllBookings = new ObservableCollection<AllBookingsView>();
+            LoadAllBookingsAsync();
+        }
+
+        public async void LoadAllBookingsAsync()
+        {
+            ObservableCollection<AllBookingsView> allBookings = await PersistancyService.LoadTableFromJsonAsync<AllBookingsView>("AllBookingsViews");
+
+            foreach (var item in allBookings)
+            {
+                this.AllBookings.Add(item);
+            }
+        }
+    }
+}
--- LokalestyringUWP/ViewModel/UserBookingsVM.cs
+++ LokalestyringUWP/ViewModel/UserBookingsVM.cs
@@ -16,41 +16,45 @@ namespace LokalestyringUWP.ViewModel
 {
     public class UserBookingsVM : INotifyPropertyChanged
     {
-        private UserBookingsView _selectedBooking;
-        public ObservableCollection<UserBookingsView> UserBookingsViewCollectionFromSingleton { get; set; }
+        private AllBookingsView _selectedBooking;
+        public ObservableCollection<AllBookingsView> AllBookingsViewSingleton { get; set; }
         public ObservableCollection<TavleBooking> Tavlebookings { get; set; }
         public UserBookingsVM()
         {
-            UserBookingsViewCollectionFromSingleton = new ObservableCollection<UserBookingsView>();
+            AllBookingsViewSingleton = new ObservableCollection<AllBookingsView>();
             Tavlebookings = new ObservableCollection<TavleBooking>();
 
-            UserBookingsViewCollectionFromSingleton = UserBookingsCatalogSingleton.Instance.UserBookings;
+            AllBookingsViewSingleton = AllBookingsViewCatalogSingleton.Instance.AllBookings;
             Tavlebookings = TavleBookingCatalogSingleton.Instance.TavleBookings;
             UserBookingsOnId(1);
         }
-        public UserBookingsView SelectedBooking { get { return _selectedBooking; } set { _selectedBooking = value; OnPropertyChanged(); } }
+        public AllBookingsView SelectedBooking { get { return _selectedBooking; } set { _selectedBooking = value; OnPropertyChanged(); } }
         
 
 
 
         public void UserBookingsOnId(int userid)
         {
-            var query = (from c in UserBookingsViewCollectionFromSingleton
-                        select c).Where(c => c.User_Id == userid).ToList();
+            var query = (from c in AllBookingsViewSingleton
+                         select c).Where(c => c.User_Id == userid).ToList();
 
-            UserBookingsViewCollectionFromSingleton.Clear();
+            AllBookingsViewSingleton.Clear();
             foreach (var item in query)
             {
-                UserBookingsViewCollectionFromSingleton.Add(item);
+                AllBookingsViewSingleton.Add(item);
             }
         }
-
+        public string CheckTavleBookingOnSelectedBooking 
+        {
+            get { return CheckIfTavleBookingExists(); }
+        }
 
         public string CheckIfTavleBookingExists()
         {
-            //var query = from t in Tavlebookings
-            //            where t.Booking_Id == SelectedBooking.boo
-            return "";
+            var query = (from t in Tavlebookings
+                         select t).Where(x => x.Booking_Id == SelectedBooking.Booking_Id);
+
+            return query.ToString();
         }
 
 
