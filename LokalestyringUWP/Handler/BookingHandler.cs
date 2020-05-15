574ff356b346768d71fbd664a0cbf82cb63256b8
Jakob-Droid <58071936+Jakob-Droid@users.noreply.github.com>
2020-05-15 11:08:45 +0200

    Login Handler og Booking.Opret.bruger :*

--- LokalestyringUWP/Handler/BookingHandler.cs
+++ LokalestyringUWP/Handler/BookingHandler.cs
@@ -10,18 +10,28 @@ namespace LokalestyringUWP.Handler
 {
     public static class BookingHandler
     {
-        public static DateTime Date { get; set; }
-        public static TimeSpan TimeStart { get; set; }
-        public static TimeSpan TimeEnd { get; set; }
-        public static int UserId { get; set; }
+        public static DateTime SelectedDate { get; set; }
+        public static TimeSpan SelectedTimeStart { get; set; }
+        public static TimeSpan SelectedTimeEnd { get; set; }
         public static int RoomId { get; set; }
         public static ICollection<TavleBooking> TavleBooking { get; set; }
         public static int BookingId { get; set; }
 
+        public static Room SelectedRoom { get; set; }
+
 
         public static void CreateBooking()
         {
-            PersistancyService.SaveBookingAsJsonAsync(new Booking(){Date = Date,Booking_Id = BookingId,Room_Id = RoomId,TavleBookings = TavleBooking, Time_end = TimeEnd, Time_start = TimeStart, User_Id = UserId});
+            PersistancyService.SaveBookingAsJsonAsync(new Booking()
+            {
+                Date = SelectedDate,
+                Booking_Id = BookingId,
+                Room_Id = SelectedRoom.Room_Id,
+                TavleBookings = TavleBooking,
+                Time_end = SelectedTimeEnd,
+                Time_start = SelectedTimeStart,
+                User_Id = LoginHandler.SelectedUser.User_Id
+            });
         }
 
 
