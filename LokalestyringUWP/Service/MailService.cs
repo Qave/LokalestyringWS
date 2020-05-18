e553e6fbd39148760e7dc0dabeab9aa808c77d4e
Magnus Hansen <magnus131hansen@gmail.com>
2020-05-18 10:46:41 +0200

    MailService

--- LokalestyringUWP/Service/MailService.cs
+++ LokalestyringUWP/Service/MailService.cs
@@ -1,12 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mail;
 using System.Text;
 using System.Threading.Tasks;
+using LokalestyringUWP.Handler;
 
 namespace LokalestyringUWP.Service
 {
-    class MailService
+    public class MailService
     {
+        public static async void MailSender(string userMail, string subject, string body)
+        {
+            await SendMail(userMail, subject, body);
+        }
+
+        private static async Task SendMail(string userMail, string subject, string body)
+        {
+            try
+            {
+                MailMessage mail = new MailMessage();
+                SmtpClient smtpServer = new SmtpClient("smtp.gmail.com");
+
+                mail.From = new MailAddress("derp43434@gmail.com");
+                mail.To.Add(userMail);
+                mail.Subject = subject;
+                mail.Body = body;
+
+                smtpServer.Port = 587;
+                smtpServer.Credentials = new System.Net.NetworkCredential("derp43434", "Derp-1234");
+                smtpServer.EnableSsl = true;
+
+                await smtpServer.SendMailAsync(mail);
+            }
+            catch (Exception ex)
+            {
+                DialogHandler.Dialog(ex.Message, "Mail fejlet");
+            }
+        }
     }
 }
